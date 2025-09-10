a6c25dcf7b85a07f4df28ef30f96a3167df78ff7
Eze0507 <xxezequielxx315@gmail.com>
2025-09-10 16:36:42 -0400

    implementacion del gestor de usuario

---
+++ personal_admin/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, GroupAuxViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+router.register(r'groupsAux', GroupAuxViewSet, basename='groupAux')
+urlpatterns = [
+    path('', include(router.urls)),
+]
--- personal_admin/views.py
+++ personal_admin/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers.serializers_user import UserSerializer, GroupAuxSerializer
+from django.contrib.auth.models import User, Group
 
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 # Create your views here.
+
+class GroupAuxViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupAuxSerializer
