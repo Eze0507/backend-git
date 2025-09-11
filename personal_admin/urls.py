98197a1b3744ed4a0587183ae8969dc4c1a40e89
Eze0507 <xxezequielxx315@gmail.com>
2025-09-11 15:20:31 -0400

    funcionalidades de registro y login de clientes

--- personal_admin/urls.py
+++ personal_admin/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, GroupAuxViewSet
+from .views import UserViewSet, GroupAuxViewSet, UserRegistrationView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'groupsAux', GroupAuxViewSet, basename='groupAux')
 urlpatterns = [
     path('', include(router.urls)),
+    path('register/', UserRegistrationView.as_view(), name='register'),
 ]
--- personal_admin/views.py
+++ personal_admin/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers.serializers_user import UserSerializer, GroupAuxSerializer
+from .serializers.serializers_register import UserRegistrationSerializer
 from django.contrib.auth.models import User, Group
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-# Create your views here.
 
 class GroupAuxViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupAuxSerializer
+
+class UserRegistrationView(APIView):
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
