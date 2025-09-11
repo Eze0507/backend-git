069e68de0524e93a1d0903c6e0fe68e087d1cced
jhamils1 <jhamil.serrudo@ficct.uagrm.edu.bo>
2025-09-10 20:25:07 -0400

    Guardando cambios locales antes de actualizar el proyecto

--- personal_admin/views.py
+++ personal_admin/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import Cargo
+from .serializers import CargoSerializer
+
+class CargoViewSet(viewsets.ModelViewSet):
+	queryset = Cargo.objects.all()
+	serializer_class = CargoSerializer
