2f6b43fde0720f3f981ea39d1049c9b73bc93e8f
jhamils1 <jhamil.serrudo@ficct.uagrm.edu.bo>
2025-10-08 10:07:16 -0400

    actualizacion2: Implementacion del models serializers views urls de Inventario

--- operaciones_inventario/modelsInventario.py
+++ operaciones_inventario/modelsInventario.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Inventario(models.Model):
+	codigo = models.CharField(max_length=50, unique=True)
+	nombre = models.CharField(max_length=100)
+	fabricante = models.CharField(max_length=100, blank=True, null=True)
 	CATEGORIA_CHOICES = [
 		("filtros", "Filtros"),
 		("frenos", "Frenos"),
@@ -12,14 +15,15 @@ class Inventario(models.Model):
 		("otros", "Otros"),
 	]
 	categoria = models.CharField(max_length=20, choices=CATEGORIA_CHOICES)
-	codigo = models.CharField(max_length=50, unique=True)
-	descripcion = models.TextField(blank=True, null=True)
-	fabricante = models.CharField(max_length=100, blank=True, null=True)
 	imagen = models.URLField(blank=True, null=True)
-	nombre = models.CharField(max_length=100)
 	precio = models.DecimalField(max_digits=10, decimal_places=2)
 	stock = models.IntegerField()
-	tipo = models.CharField(max_length=50)
+	descripcion = models.TextField(blank=True, null=True)	
+	TIPO_CHOICES = [
+		("venta", "Ítem de venta"),
+		("almacen", "Ítem de almacén"),
+	]
+	tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
 
 	def __str__(self):
 		return f"{self.nombre} ({self.codigo})"
--- operaciones_inventario/serializerInventario.py
+++ operaciones_inventario/serializerInventario.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .modelsInventario import Inventario
 
-
 class InventarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventario
-        fields = ['id', 'categoria', 'codigo', 'descripcion', 'fabricante', 'imagen', 'nombre', 'precio', 'stock', 'tipo']
+        fields = '__all__'
