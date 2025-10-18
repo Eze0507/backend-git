3a63320fb5e4fc139c614b97463db21651bc06ba
jhamils1 <jhamil.serrudo@ficct.uagrm.edu.bo>
2025-10-18 16:06:56 -0400

    Configuración final verificada para Railway

--- backend_taller/settings.py
+++ backend_taller/settings.py
@@ -116,7 +116,10 @@ WSGI_APPLICATION = 'backend_taller.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DJANGO_DATABASE_URL')) 
+    'default': dj_database_url.config(
+        default=config('DJANGO_DATABASE_URL', default='sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # Password validation
