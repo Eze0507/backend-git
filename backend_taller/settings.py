dfdee0d1f323fbba3cddab2018b704aa2840ac9e
jhamils1 <jhamil.serrudo@ficct.uagrm.edu.bo>
2025-10-18 18:00:20 -0400

    Fix: Cambiar DJANGO_DATABASE_URL a DATABASE_URL

--- backend_taller/settings.py
+++ backend_taller/settings.py
@@ -117,7 +117,7 @@ WSGI_APPLICATION = 'backend_taller.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=config('DJANGO_DATABASE_URL', default='sqlite:///db.sqlite3'),
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
