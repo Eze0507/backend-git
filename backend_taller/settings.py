774e12ad69bed1b787da4144495526a07d771b0b
jhamils1 <jhamil.serrudo@ficct.uagrm.edu.bo>
2025-10-18 12:17:53 -0400

    Config despliegue 2 Cambio variable DJANGO_SECRET_KEY para Railway y local

--- backend_taller/settings.py
+++ backend_taller/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-
+#SECRET_KEY = config('SECRET_KEY') <---Antes porque daba error en despliegue
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
