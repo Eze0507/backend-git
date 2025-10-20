c58de9cba8c299b64d77645b0f39181c48307fb4
Eze0507 <xxezequielxx315@gmail.com>
2025-10-20 10:06:35 -0400

    update

--- backend_taller/settings.py
+++ backend_taller/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["192.168.0.3","localhost","127.0.0.1"]
+ALLOWED_HOSTS = ["localhost","127.0.0.1", "192.168.0.13"]
 
 # Application definition
 
