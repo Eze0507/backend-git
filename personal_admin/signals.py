b58cfbdb3cb8dc9b3b5c6b3689b3bf060df848ac
Developer <luis20arevalo20@gmail.com>
2025-11-06 04:35:04 -0400

    Solucion error usuarios

--- personal_admin/signals.py
+++ personal_admin/signals.py
@@ -2,15 +2,3 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models_saas import UserProfile, Tenant
-
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance)
-        
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    if hasattr(instance, 'profile'):
-        instance.profile.save()
-    else:
-        UserProfile.objects.create(user=instance)
