ae0378ab4b5c38f25e99f50afa91b710e6134ccd
Developer <luis20arevalo20@gmail.com>
2025-11-06 03:07:38 -0400

    Actualizacion error reportes

--- servicios_IA/urls.py
+++ servicios_IA/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import AlprScanView
 from .viewsReportes import ReporteViewSet
+from .views_chatbot import GeminiChatView
 
 # Router para ViewSets
 router = DefaultRouter()
@@ -10,11 +11,6 @@ router.register(r'reportes', ReporteViewSet, basename='reporte')
 
 urlpatterns = [
     path("alpr/", AlprScanView.as_view(), name="alpr-scan"),
-    path("", include(router.urls)),
-from .views_chatbot import GeminiChatView
-
-
-urlpatterns = [
-    path("alpr/", AlprScanView.as_view(), name="alpr-scan"),
     path("chatbot/", GeminiChatView.as_view(), name="chatbot"),
+    path("", include(router.urls)),
 ]
